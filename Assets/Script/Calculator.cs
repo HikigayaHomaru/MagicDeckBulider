517dbfdb66859f1cbfb4631e54e027a6c52f3f4d
Homaru <D.K.R@hotmail.com>
2018-07-11 09:04:55 +0900

    解决bug

--- Assets/Script/Calculator.cs
+++ Assets/Script/Calculator.cs
@@ -4,34 +4,44 @@ using UnityEngine;
 
 public class Calculator : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("HypDist(4, 10, 40, 100) = " + HypDist(4, 10, 40, 100));//Bug exist.
-        //Debug.Log(Choices(9, 6));//Succeed.
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start() {
+        Debug.Log("超几何分布(4,10,50,99) = " + HypDist(7, 13, 39, 99));//Bug exist.
+                                                                        //Debug.Log(Choices(9, 6));//Succeed.
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 
     /// <summary>
     /// Using to calculate Hypergeometric distribution(超几何分布).
     /// </summary>
     /// <returns></returns>
-    private float HypDist(int k, int n, int m, int N)
+    private decimal HypDist(ulong k, ulong n, ulong m, ulong N)
     {
-        return (Choices(m, k) * Choices(N - m, n - k)) / Choices(N, n);
+        ulong a = _nCm(m, k);
+        ulong b = _nCm(N - m, n - k);
+        decimal c = (decimal)(a * b);
+        ulong d = _nCm(N, n);
+        //return (Choices(m, k) * Choices(N - m, n - k)) / Choices(N, n);
+        //Debug.Log("_nCm(m, k) = " + a);
+        //Debug.Log("_nCm(N - m, n - k) = " + b);
+        //Debug.Log("a * b = " + c);
+        //Debug.Log("_nCm(N, n) = " + d);
+        //Debug.Log("c / d = " + c / d);
+        return 1-(c / d);
     }
 
-
+    /*
     /// <summary>
     /// Using to calculate Binomial coefficient(二項式係數/組合/選擇).
     /// </summary>
     /// <param name="n"></param>
     /// <param name="k"></param>
     /// <returns></returns>
-    private int Choices(int n,int k)
+    private ulong Choices(ulong n, ulong k)
     {
         return Factorial(n) / (Factorial(k) * Factorial(n - k));
         Debug.Log("Choices is running.");
@@ -42,27 +52,31 @@ public class Calculator : MonoBehaviour {
     /// </summary>
     /// <param name="x"></param>
     /// <returns></returns>
-    private int Factorial(int x)
+    private ulong Factorial(ulong x)
     {
         if (x > 0)
         {
-            int n = 1;
-            for (int i = 2; i <= x; i++)
+            ulong n = 1;
+            for (ulong i = 2; i <= x; i++)
             {
                 n *= i;
             }
-            //Debug.Log("Factorial is returned.");
+            Debug.Log("Factorial is returned.");
+            Debug.Log(n);
             return n;
-        }
-        else if (x == 0)
-        {
-            //Debug.Log("Factorial is 0.");
-            return 1;
         }
         else
         {
-            Debug.Log("Error. Num cant be small then 0.");//Return an Error.
-            return -1;
+            Debug.Log("Factorial is 0.");
+            return 1;
         }
+    }*/
+
+    
+    private ulong _nCm(ulong n,ulong m)
+    {
+        if (m == 0) return 1;
+        if (n == 0) return 0;
+        return n * _nCm(n - 1, m - 1) / m;
     }
 }
