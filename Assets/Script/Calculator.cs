8bcc2221b9030dfffb8f89f3485bf3bf38234808
Homaru <D.K.R@hotmail.com>
2018-07-13 07:10:25 +0900

    增加计算器功能（众数，期望）

--- Assets/Script/Calculator.cs
+++ Assets/Script/Calculator.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class Calculator : MonoBehaviour {
 
-    // Use this for initialization
-    void Start() {
-        Debug.Log("超几何分布(4,10,50,99) = " + HypDist(7, 13, 39, 99));//Bug exist.
-                                                                        //Debug.Log(Choices(9, 6));//Succeed.
-    }
+    public ulong Extrac;//抽取数
+    public ulong Manu;//合格样本
+    public ulong Num;//样本总数
 
+    // Use this for initialization
+    void Start()
+    {
+        int mode = Mode(Extrac, Manu, Num);
+        float fal = 0;
+        Debug.Log("期望值为" + Mean(Extrac, Manu, Num));
+        Debug.Log("众数为" + mode);
+        for(ulong i = 0; i <= Extrac; i++)
+        {
+            decimal hyp = HypDist(i, Extrac, Manu, Num);
+            Debug.Log("超几何分布" + i + "=" + hyp);
+            if (i < (ulong)mode)
+            {
+                fal += (float)hyp;
+            }
+        }
+
+
+        Debug.Log("小于" + mode + "概率为" + fal * 100 + "%");
+
+
+        //Debug.Log("超几何分布(4,10,50,99) = " + HypDist(7, 13, 39, 99));
+    }
     // Update is called once per frame
     void Update() {
 
+    }
+
+    /// <summary>
+    /// Using to calculate Mode(众数).
+    /// </summary>
+    /// <returns></returns>
+    public int Mode(ulong n, ulong m, ulong N)
+    {
+        int x = (int)n + 1;
+        int y = (int)m + 1;
+        int z = (int)N + 2;
+        return ((x * y) / z);
+    }
+        
+        
+    /// <summary>
+    /// Using to calculate Mean(期望值).
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="m"></param>
+    /// <param name="N"></param>
+    /// <returns></returns>
+    private float Mean(ulong n, ulong m, ulong N)
+    {
+        float x = (float)n * (float)m / (float)N;
+        return x;
     }
 
     /// <summary>
@@ -31,48 +78,15 @@ public class Calculator : MonoBehaviour {
         //Debug.Log("a * b = " + c);
         //Debug.Log("_nCm(N, n) = " + d);
         //Debug.Log("c / d = " + c / d);
-        return 1-(c / d);
+        return (c / d);
     }
-
-    /*
+    
     /// <summary>
     /// Using to calculate Binomial coefficient(二項式係數/組合/選擇).
     /// </summary>
     /// <param name="n"></param>
-    /// <param name="k"></param>
+    /// <param name="m"></param>
     /// <returns></returns>
-    private ulong Choices(ulong n, ulong k)
-    {
-        return Factorial(n) / (Factorial(k) * Factorial(n - k));
-        Debug.Log("Choices is running.");
-    }
-
-    /// <summary>
-    /// Using to calculate Factorial(階乗).
-    /// </summary>
-    /// <param name="x"></param>
-    /// <returns></returns>
-    private ulong Factorial(ulong x)
-    {
-        if (x > 0)
-        {
-            ulong n = 1;
-            for (ulong i = 2; i <= x; i++)
-            {
-                n *= i;
-            }
-            Debug.Log("Factorial is returned.");
-            Debug.Log(n);
-            return n;
-        }
-        else
-        {
-            Debug.Log("Factorial is 0.");
-            return 1;
-        }
-    }*/
-
-    
     private ulong _nCm(ulong n,ulong m)
     {
         if (m == 0) return 1;
